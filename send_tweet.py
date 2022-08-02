850b9da5230feccb4ba6e9047257c41e97a772b0
Dan Jutan <danjutan@gmail.com>
2022-08-02 20:05:42 +0000

    correct arg name

--- send_tweet.py
+++ send_tweet.py
@@ -1,7 +1,7 @@
 import tweepy
 import sys
 
-client = tweepy.Client(consumer_secret=sys.argv[1], consumer_key=sys.argv[2], access_token_key=sys.argv[3], access_token_secret=sys.argv[4])
+client = tweepy.Client(consumer_secret=sys.argv[1], consumer_key=sys.argv[2], access_token=sys.argv[3], access_token_secret=sys.argv[4])
 tweet = "I wrote this, and I'm not human!"
 url = sys.arv[5]
 response = client.create_tweet(text=tweet + " " + url)
